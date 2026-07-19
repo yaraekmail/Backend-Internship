4d3effbdf54a0d050113d415b127ef33efe75cf4
yaraekmail <yaraeyadkmail272@gmail.com>
2026-07-19 18:34:56 +0300

    update day 1 readme

---
+++ Day1/README.md
@@ -0,0 +1,20 @@
+# Backend Internship - Day 1
+
+## About
+This repository contains my tasks and practice during the Backend Internship.
+
+## Day 1 Tasks
+- Set up the .NET environment.
+- Created my first C# Console Application.
+- Practiced basic C# syntax and console output.
+- Learned how to use Git and GitHub for version control.
+
+## Technologies
+- C#
+- .NET
+- Git
+- GitHub
+
+## Author
+Yara Kmail  
+Arab American University
--- Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
+++ Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23f3eed496b4d1fb35a8ecbbd907177a86b690ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
